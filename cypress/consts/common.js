3ae3dcf8bc422a05fe9b94dc25dea50d484f6d82
andriisolonchuk <andriisolonchuk@gmail.com>
2019-06-25 20:14:10 +0300

    visibility test

--- cypress/consts/common.js
+++ cypress/consts/common.js
@@ -2,10 +2,16 @@ export const commonConsts = {
     locators: {
         usernameInput: '#username',
         usernamePassword: '#password',
-        loginButton: '#kc-login'
+        loginButton: '#kc-login',
+        loginFailedMessage:'h1',
+        rememberMeCheckBox: '#rememberMe',
+        forgotPasswordLink: '.forgot-password-href',
+        registerLink: '#register-link',
+        supportLink: '#support-link'
     },
   strings:{
         fakeUsername: 'fakeUsername@email.com',
-        fakePassword: 'fakePassword'
+        fakePassword: 'fakePassword',
+        loginFailedMessage: '405 Not Allowed'
   }
 };
--- cypress/integration/firstTest.js
+++ cypress/integration/firstTest.js
@@ -3,15 +3,30 @@ import {commonConsts} from "../consts/common";
 context('Actions', () => {
     beforeEach(() => {
         cy.visit('https://staging-wcs.c-b4.com/testme/#/login');
+        //todo check url and status code
     });
 
-    it('Checking negative scenario', () => {
+    it('Checking that page elements are visible', ()=>{
+        cy.get(commonConsts.locators.usernameInput).should('be.visible');
+        cy.get(commonConsts.locators.usernamePassword).should('be.visible');
+        cy.get(commonConsts.locators.loginButton).should('be.visible');
+        cy.get(commonConsts.locators.rememberMeCheckBox).should('be.visible');
+        cy.get(commonConsts.locators.forgotPasswordLink).should('be.visible');
+        cy.get(commonConsts.locators.registerLink).should('be.visible');
+        cy.get(commonConsts.locators.supportLink).should('be.visible');
+    });
+
+    it('Checking negative scenario of the login process', () => {
         cy.get(commonConsts.locators.usernameInput)
             .type(commonConsts.strings.fakeUsername)
             .get(commonConsts.locators.usernamePassword)
             .type(commonConsts.strings.fakePassword)
             .get(commonConsts.locators.loginButton)
-            .click();
+            .click() //todo check status code
+            .get(commonConsts.locators.loginFailedMessage)
+            .should('contain', commonConsts.strings.loginFailedMessage);
     });
+
+
 
 });

6eabbcf4363fa915fada35a6a2cf5bd644407791
andriisolonchuk <andriisolonchuk@gmail.com>
2019-06-25 21:56:15 +0300

    SupportPage -> Cheching all neccessary fields + description

--- cypress/integration/supportPage.js
+++ cypress/integration/supportPage.js
@@ -1,5 +1,13 @@
 import {commonConsts} from "../consts/common";
 
+const checkValidationErrorVisisble = () => {
+    return  cy.get(commonConsts.locators.submitButton)
+        .click()
+        .get(commonConsts.locators.validationError)
+        .should('be.visible')
+        .and('contain', commonConsts.strings.emptyFieldsAllErrorMessage);
+};
+
 context('Actions', () => {
     before(() => {
         cy.visit(commonConsts.urls.supportPageUrl);
@@ -17,21 +25,37 @@ context('Actions', () => {
     context('Cheching all neccessary fields are filled', () => {
         it('Checking your name field', () => {
              cy.get(commonConsts.locators.yourNameInput)
-                .type(commonConsts.strings.yourName)
-                .get(commonConsts.locators.submitButton).click()
-                 .get(commonConsts.locators.validationError)
-                 .should('be.visible')
-                 .and('contain', commonConsts.strings.emptyFieldsAllErrorMessage);
+                 .type(commonConsts.strings.yourName);
+            checkValidationErrorVisisble();
         });
 
         it('Checking organization field', () => {
             cy.get(commonConsts.locators.organizationNameInput)
-                .type(commonConsts.strings.organizationName)
-                .get(commonConsts.locators.submitButton).click()
+                .type(commonConsts.strings.organizationName);
+            checkValidationErrorVisisble();
+        });
+
+        it('Checking region field', () => {
+            cy.get(commonConsts.locators.regionInput)
+                .type(commonConsts.strings.region);
+            checkValidationErrorVisisble();
+        });
+
+        it('Checking email field', () => {
+            cy.get(commonConsts.locators.contactEmailInput)
+                .type(commonConsts.strings.fakeUsername);
+            checkValidationErrorVisisble();
+        });
+
+        it('Checking description field', () => {
+            cy.get(commonConsts.locators.descriptionInput)
+                .type(commonConsts.strings.description)
+                .get(commonConsts.locators.submitButton)
+                .click()
                 .get(commonConsts.locators.validationError)
-                .should('be.visible')
-                .and('contain', commonConsts.strings.emptyFieldsAllErrorMessage);
+                .should('not.be.visible');
         });
     });
+
 
 });
